1335550419817cfeaddafac5320f64be12563494
eric <xsephiroth@foxmail.com>
2020-10-23 10:14:35 +0800

    Category选中与待删除逻辑优化

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,56 +1,76 @@
-import React, { useState } from "react";
-import { useRecoilValue } from "recoil";
-import { useRecoilState } from "recoil";
+import React, { useCallback } from "react";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import { Layout, NavigationBar, CategoryBtn, AmountInput } from "../components";
-import { categoriesState, categorySelectState } from "../store/category";
+import {
+  categoriesState,
+  categoryDeleteSelectState,
+  isCategoryChoiceSelector,
+  isWillDeleteCategorySelector,
+} from "../store/category";
 
-const Categories = styled.div`
+const StyledCategories = styled.div`
   height: 50vh;
 `;
 
-const Edit = () => {
-  const categories = useRecoilValue(categoriesState);
-  const [categorySelect, setCategorySelect] = useRecoilState(
-    categorySelectState
+const Btn = ({ category }) => {
+  const [active, setCategorySelect] = useRecoilState(
+    isCategoryChoiceSelector(category)
   );
-  const [categoryDeleteSelect, setCategoryDeleteSelect] = useState("");
-  console.log({ categoryDeleteSelect });
+  const [willDelete, setCategoryDeleteSelect] = useRecoilState(
+    isWillDeleteCategorySelector(category)
+  );
 
-  const clearCategoryDeleteSelect = () => {
-    // 仅在删除时起作用
-    if (!categoryDeleteSelect) return;
-    setCategorySelect("");
-    setCategoryDeleteSelect("");
-  };
+  const handleClick = useCallback(
+    (e) => {
+      e.stopPropagation();
+      setCategorySelect(category);
+    },
+    [category, setCategorySelect]
+  );
 
-  const handleCategoryLongPress = (category) => {
-    setCategorySelect("");
-    setCategoryDeleteSelect(category);
-  };
+  const handleLongPress = useCallback(() => setCategoryDeleteSelect(category), [
+    category,
+    setCategoryDeleteSelect,
+  ]);
 
+  return (
+    <CategoryBtn
+      active={active}
+      onClick={handleClick}
+      willDelete={willDelete}
+      setLongPress={handleLongPress}
+    >
+      {category}
+    </CategoryBtn>
+  );
+};
+
+const Categories = () => {
+  const categories = useRecoilValue(categoriesState);
+
+  // 点击空白处取消删除的选中
+  // 不影响正常选中的类目
+  const setCategoryDeleteSelect = useSetRecoilState(categoryDeleteSelectState);
+  const clearWillDeleteSelect = () => setCategoryDeleteSelect("");
+
+  return (
+    <StyledCategories onClick={clearWillDeleteSelect}>
+      {categories.map((category) => (
+        <Btn key={category} category={category} />
+      ))}
+    </StyledCategories>
+  );
+};
+
+const Edit = () => {
   return (
     <Layout>
       <NavigationBar
         start={<NavigationBar.Back />}
         center={<NavigationBar.Center>Hello</NavigationBar.Center>}
       />
-      <Categories onClick={clearCategoryDeleteSelect}>
-        {categories.map((c) => (
-          <CategoryBtn
-            key={c}
-            active={categorySelect === c}
-            onClick={(e) => {
-              e.stopPropagation();
-              setCategorySelect((prev) => (prev === c ? "" : c));
-            }}
-            willDelete={categoryDeleteSelect === c}
-            setLongPress={() => handleCategoryLongPress(c)}
-          >
-            {c}
-          </CategoryBtn>
-        ))}
-      </Categories>
+      <Categories />
       <AmountInput />
     </Layout>
   );
