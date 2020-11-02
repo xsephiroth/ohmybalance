ea6594898a040ae87cf476122e49880b6289ef49
eric <xsephiroth@foxmail.com>
2020-11-02 12:25:52 +0800

    page padding

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   background-color: ${({ theme }) => theme.backgroundColor.primary};
+  padding: 10px 0;
   min-height: 100vh;
 `;
 
